764e597ac573fd7d523c4c70e87dd20a694f2151
leandro fusco <leandrofusdev@gmail.com>
2023-05-20 22:36:22 -0300

    ok

--- routes/stories.js
+++ routes/stories.js
@@ -5,10 +5,27 @@ const { ObjectId } = require('mongodb');
 const menuFill = require('./init')().then((res)=>{
     return res
   });
-router.get('/s/:story', async function (req, res, _next) {
+  router.get('/s/:story', async function (req, res, _next) {
     let a = await db.getDb('sagpic_lector','stories',{_id:new ObjectId(req.params.story)})
     console.log(a);
 res.send(a)
+});
+
+router.get('/s/publish/new', async function (req, res, _next) {
+  let a = await db.getDb('sagpic_lector','stories')
+  res.render('layouts/profile',
+  {
+    title: 'Sagpic - Perfil',
+    loggedIn:req.session.user,
+    menu:await menuFill,
+    publish:true,
+    
+    //user: me,
+    //profile:true,
+    //me:true,
+    //storyMeta: storyMeta,
+  },
+);
 });
 
 module.exports = router
